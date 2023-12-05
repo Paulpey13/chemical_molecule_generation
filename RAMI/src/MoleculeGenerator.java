8243b67a3f93e23d080173df182e4a5f6f6f8a21
Paulpey13 <paul.peyssard@gmail.com>
2023-12-05 15:04:33 +0100

    Valence dico improvment

--- RAMI/src/MoleculeGenerator.java
+++ RAMI/src/MoleculeGenerator.java
@@ -2,35 +2,33 @@ import org.chocosolver.solver.Model;
 import org.chocosolver.solver.variables.IntVar;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Arrays;
+
 
 public class MoleculeGenerator {
-    private static final Map<String, Integer> VALENCE_MAP = new HashMap<String, Integer>() {{
-        put("H", 1);  // Hydrogène
-        put("He", 0); // Hélium
-        put("Li", 1); // Lithium
-        put("Be", 2); // Béryllium
-        put("B", 3);  // Bore
-        put("C", 4);  // Carbone
-        put("N", 3);  // Azote
-        put("O", 2);  // Oxygène
-        put("F", 1);  // Fluor
-        put("Ne", 0); // Néon
-        put("Na", 1); // Sodium
-        put("Mg", 2); // Magnésium
-        put("Al", 3); // Aluminium
-        put("Si", 4); // Silicium
-        put("P", 3);  // Phosphore
-        put("S", 2);  // Soufre
-        put("Cl", 1); // Chlore
-        put("Ar", 0); // Argon
-        put("K", 1);  // Potassium
-        put("Ca", 2); // Calcium
-        // ... continuez pour les autres éléments ou voir si y'a pas une meilleur solution
-    }};
 
+    //Dictionnaire de valence pour chaques atomes
+    private static final Map<String, Integer> VALENCE_MAP = new HashMap<>();
 
+    static {
+        addToMap(new String[]{"He", "Ne", "Ar"}, 0);
+        addToMap(new String[]{"Li", "Na", "K","H", "F", "Cl"}, 1);
+        addToMap(new String[]{"Be", "Mg", "Ca","O", "S", "Se"}, 2);
+        addToMap(new String[]{"B", "Al", "Ga","N", "P", "As"}, 3);
+        addToMap(new String[]{"C", "Si", "Ge"}, 4);
+        //Y'a pas tout il faut finir de rajouter
+
+        //La contrainte devra donc faire respecter chaque atome et sa valeur dans ce dico
+    }
+
+    //Pour ajouter des elements au dico de valence sans faire 10000lignes
+    private static void addToMap(String[] elements, int valence) {
+        for (String element : elements) {
+            VALENCE_MAP.put(element, valence);
+        }
+    }
 
     public static void main(String[] args) {
-        System.out.println("La valence du varbonne est : "+VALENCE_MAP.get("C"));
+        System.out.println("La valence du carbonne est : "+VALENCE_MAP.get("C"));
     }
 }
