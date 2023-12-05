7100f43b30c59bf9b7f7047918ac47f0a85507d6
Paulpey13 <paul.peyssard@gmail.com>
2023-12-05 14:32:56 +0100

    Librairies working

--- RAMI/src/Main.java
+++ RAMI/src/Main.java
@@ -11,7 +11,7 @@ public class Main {
         for (int q = 0; q < n; q++) {
             vars[q] = model.intVar("Q_" + q, 1, n);
         }
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 1; i < n - 1; i++) {
             for (int j = i + 1; j < n; j++) {
                 model.arithm(vars[i], "!=", vars[j]).post();
                 model.arithm(vars[i], "!=", vars[j], "-", j - i).post();
